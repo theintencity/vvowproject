a8577d1ee0cb237e43a37c2af5c74a048ebe04e5
theintencity <theintencity@dcac32d1-f45a-414a-f75b-11e3c1bf3930>
2012-07-29 14:00:53 -0700

    Refactor as follows: - allow other DBs: sqlite3 default, postgresql, and inmemory. - interface for resource/db structure. - cleanup notifier

--- server/restserver_wsh.py
+++ server/restserver_wsh.py
@@ -1,5 +1,5 @@
 # The Python version of restserver uses local sqlite3 database file, and restserver.py and ajaxserver.py
-# modules for synchronouse and asynchronous requests. The server also acts as a web-server for local files.
+# modules for synchronous and asynchronous requests. The server also acts as a web-server for local files.
 # This poses security risk, because the database file is also available in current directory.
 # Feel free to re-organize the base server for reducing the security risk.
 #
@@ -16,141 +16,125 @@
 #
 
 import sys, logging, traceback, json, threading
-from mod_pywebsocket import msgutil
-from restserver import Database, Handler
+from mod_pywebsocket import msgutil, handshake
+from restserver import Sqlite3Database as Database, Handler
+# alternatively: use PostgreSQLDatabase or InmemoryDatabase
 
 logger = logging.getLogger('restserver')
+databases = {}
 
-
-db = Database()
-db.reset()
+for name in ('restserver', 'private'):
+    try:
+        db = Database(database=name)
+        db.reset()
+        databases[name] = db;
+    except: pass
 
 clients = {}
 lock = threading.Lock()
         
 class Client(Handler):
-    def __init__(self, request):
+    def __init__(self, request, db):
         Handler.__init__(self, db)
         self.request = request
         self.id = str(id(self))
         clients[self.id] = self
+        logger.info('%s connection created, uri %s', self.id, request.uri)
 
     def send(self, data):
         try:
+            logger.info('%s send %s', self.id, data);
             self.request.ws_stream.send_message(data, binary=False)
         except:
-            logger.error("send failed %s"%(sys.exc_info()[1],))
+            logger.error("%s send failed %s", self.id, sys.exc_info()[1])
+            #traceback.print_exc()
             self.close()
 
     def received(self, data):
-        logger.debug('received ' + data)
+        logger.info('%s received %s', self.id, data)
         request = json.loads(data)
         
         if 'method' not in request or 'resource' not in request or 'msg_id' not in request:
-            logger.warn('missing mandatory property')
+            logger.warn('%s missing mandatory property', self.id)
             return
         
         method, resource = request['method'], request['resource']
-        if method in ['POST', 'PUT', 'GET', 'DELETE', 'SUBSCRIBE', 'UNSUBSCRIBE', 'NOTIFY']:
+        if not self.allowed(method, resource, request):
+            response = {'code': 'failed', 'reason': 'unauthorized resource or method'}
+        elif method not in ['POST', 'PUT', 'GET', 'DELETE', 'SUBSCRIBE', 'UNSUBSCRIBE', 'NOTIFY']:
+            response = {"code": "failed", "reason": "unknown command %s %s"%(method, resource)}
+        else:
             try:
                 response = eval('self.%s'%(method,))(request) or {"code": "failed", "reason": "method did not return"}
             except:
-                logger.exception("exception")
+                logger.exception("%s exception in %s %s", self.id, method, resource)
                 response = {"code": "failed", "reason": "server programming exception"}
-        else:
-            response = {"code": "failed", "reason": "unknown command " + method + " " + resource}
         response['msg_id'] = request['msg_id']
+        logger.info('%s %s %s (msg-id=%r code=%r)', self.id, method, resource, request['msg_id'], response.get('code', None))
         self.send(json.dumps(response))
 
     def close(self):
-        if clients[self.id]:
+        if self.id in clients and clients[self.id]:
             del clients[self.id]
             self.request = None
-            self.db.commit('DELETE FROM subscribe WHERE cid=?', (self.id, ))
-            resources = self.db.fetchall('SELECT rid FROM resource WHERE cid=?', (self.id,))
-            self.db.commit('DELETE FROM resource WHERE cid=?', (self.id,))
-            for row in resources:
-                self.NOTIFY(row[0], 'DELETE')
+            Handler.close(self)
         return dict(code='success')
 
     def NOTIFY(self, request, method=None):
-        if method: # notification due to POST, PUT or DELETE
-            resource = request
-            # TODO: change 'from': self.id in the php code too.
-            notify = {'notify': method, 'resource': resource, 'type': None, 'entity': None, 'from': self.id}
-            if method == 'PUT' or method == 'POST':
-                try:
-                    result = self.db.fetchone('SELECT type, entity FROM resource WHERE rid=?', (resource,))
-                except:
-                    traceback.print_exc()
-                    return dict(code='failed', reason='failed to get this resource')
-                if result:
-                    notify['type'], entity = result[0], json.loads(result[1])
-                    notify['entity'] = dict([(k, v) for k, v in entity.iteritems() if not k or k[0] != "_"])
-            # TODO: also send to parent resource
-        else:
-            notify = {'notify': 'NOTIFY', 'resource': request['resource'], 'data': request['data'], 'from': self.id}
-    
-        param = json.dumps(notify)
-        
-        try:
-            result = self.db.fetchall('SELECT cid FROM subscribe WHERE rid=?', (notify['resource'],))
-        except:
-            traceback.print_exc()
-            return dict(code='failed', reason='failed to get this resource subscribers')
-        
         sent_count = 0
-        for row in result:
-            target = self.getuserbyid(row[0])
-            if not target:
-                logger.debug('invalid user for %r', row[0])
-            else:
-                target.send(param)
-                sent_count += 1
-        
-        if method in ('POST', 'PUT', 'DELETE'):
-            parent = self.get_parent(notify['resource'])
-            change = {'notify': 'UPDATE', 'resource': parent, 'type': notify['type'], 'entity': notify['entity']}
-            child = notify['resource']
-            index = child.rfind('/')
-            if index >= 0:
-                child = child[index+1:]
-            change[{'POST': 'create', 'PUT': 'update', 'DELETE': 'delete'}.get(method)] = child
-            result = self.db.fetchall('SELECT cid FROM subscribe WHERE rid=?', (parent,))
-            param = json.dumps(change)
-            
-            logger.debug('change=%r param=%r', change, param)
-            for row in result:
-                target = self.getuserbyid(row[0])
+        try:
+            for userid, param in self.notifier(request, method):
+                target = self.getuserbyid(userid)
                 if not target:
-                    logger.debug('invalid user for %r', row[0])
+                    logger.debug('%s invalid user for %r', self.id, userid)
                 else:
+                    logger.info('%s NOTIFY %r (userid=%r)', self.id, param, userid)
                     target.send(param)
                     sent_count += 1
+        except ValueError:
+            return dict(code='failed', reason=str(sys.exc_info()[1]))
         if not sent_count:
-            logger.debug('notify could not send to anyone')
+            logger.debug('%s notify could not send to anyone', self.id)
             return dict(code='failed', reason='no available user to send notification to')
-        logger.debug('notify sent to %r items', sent_count)
+        logger.debug('%s notify sent to %r items', self.id, sent_count)
         return dict(code='success', sent_count=sent_count)
         
     def getuserbyid(self, userid):
         return clients.get(userid, None)
-
     
+    def allowed(self, method, resource, request):
+        return True
+
+
 def web_socket_do_extra_handshake(request):
+    global databases
+    
     # This example handler accepts any request. See origin_check_wsh.py for how
     # to reject access from untrusted scripts based on origin value.
-    pass  # Always accept.
+    lock.acquire()
+    try:
+        database = 'restserver'
+        logger.debug("database=%r", database)
+        if database not in databases:
+            databases[database] = Database(database=database)
+        request.db = databases[database]
+        request.dbname = database
+        # request.db.reset() # TODO: need to cleanup on startup
+    except:
+        raise handshake.AbortedByUserException("Unauthorized")
+    finally:
+        lock.release()
+    #pass  # Always accept.
 
 
 def web_socket_transfer_data(request):
-    request.ws_client = Client(request)
+    request.ws_client = Client(request, request.db)
     
     try:
         while True:
             line = request.ws_stream.receive_message()
             if line is None:
-                return
+                break
             lock.acquire()
             try:
                 request.ws_client.received(line)
@@ -159,14 +143,51 @@ def web_socket_transfer_data(request):
             finally:
                 lock.release()
     except msgutil.ConnectionTerminatedException, e: # Connection closed unexpectedly
-        logger.error("ConnectionTerminatedException")
+        logger.info("ConnectionTerminatedException")
+        # traceback.print_exc()
     
+    lock.acquire()
     try:
-        lock.acquire()
         request.ws_client.close()
     except:
-        traceback.print_exc()
-    finally:
-        lock.release()
+        logger.exception("exception in cleanup")
+        # TODO: experimental code to reset this database
+        try:
+            if request.db:
+                request.db.reconnect()
+        except:
+            pass
+    
+    #try:
+    #    if request.dbname in databases:
+    #        databases[request.dbname]["count"] = databases[request.dbname]["count"] - 1
+    #        if databases[request.dbname]["count"] == 0:
+    #            db = databases[request.dbname]["db"]
+    #            del databases[request.dbname]
+    #            db.close()
+    #except:
+    #    logger.error("exception in closing the database")
+    ##request.db.close();
+    
+    lock.release()
     # del request.ws_client
+    request.db = None
 
+#def web_socket_passive_closing_handshake(request):
+#    return None, ''
+#    #lock.acquire()
+#    #try:
+#    #    request.ws_client.close()
+#    #except:
+#    #    try:
+#    #        if request.db:
+#    #            request.db.reconnect()
+#    #    except:
+#    #        pass
+#    #lock.release()
+#    #request.db = None
+#    #
+#    ## Simply echo a close status code
+#    #code, reason = request.ws_close_code, request.ws_close_reason
+#    #return code, reason
+#
